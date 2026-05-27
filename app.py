cce3ce249acbafa02ecef50a72cf9b7f5da83b34
k2009dutta <kanishka.dutta@gmail.com>
2026-05-27 17:58:35 +0530

    Version 2 deployment

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    print ("This is version 2 of the application!")
     return "GitHub Actions & Kubernetes Rolling Deployment Successful"
 
 if __name__ == "__main__":
